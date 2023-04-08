b8d842554e8f9245088ec6f433abdf38f907054a
pacleb <pacleb@gmail.com>
2023-04-08 21:26:54 +0800

    more

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
+const express = require("express");
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 app.get("/console", (req, res) => {
   res.sendFile(__dirname + "/console.html");
 });
+
+app.use("/images", express.static("images"));
 
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
